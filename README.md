4d00bdfa77cac9f22cd497d4666414845519e242
Ruben Berenguel <ruben@mostlymaths.net>
2024-07-23 00:32:29 +0200

    Let's see if it works on mobile

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # nb
 
 A very simple PWA for the [n-back task](https://en.wikipedia.org/wiki/N-back), visual-only.
+Has double and triple modes.
 
 A long time ago I found an app I liked for it, with only positions and letters. Most are
 position+audio, which I don't like. But that stopped working, so…
@@ -9,3 +10,32 @@ position+audio, which I don't like. But that stopped working, so…
 I have the feeling the n-back task is useful for focus. Otherwise, it was a neat side
 project to hack together in a couple afternoons.
 
+## Usage
+
+> [!NOTE]
+> This should work best on a phone in portrait, it could look weird otherwise.
+
+To change the depth level press any of the squares (assume they are 1-9), the number in the top (the `n`-level) 
+will change accordingly. Pressing the same square twice switches from dual to triple N-back.
+
+To start, tap the top, where the level appears. To pause while playing, press the middle or top. To dismiss the pause,
+tap the modal and then either choose a different level or just start. It will begin from "now" (so, no history),
+but will keep your current score.
+
+The answer buttons (details below) won't appear until an answer is expected. The answer buttons will briefly flash red or green depending on whether you were right or wrong.
+
+### Dual
+
+- You need to remember colors and positions.
+- If it's the same position as `n`-steps before, tap the middle left side, it will change color when selected. Press again to undo the answer.
+- If it's the same color as `n`-steps before, tap the middler right side, it will change color when selected. Press again to undo the answer.
+
+### Triple
+
+- You need to remember colors, positions and letters.
+- For position and color, as in dual.
+- For letter, press the bottom.
+
+## Tip(s)
+
+If you want to see all possible colors, visit the page with the URL ending in `?colors`.
--- nb.js
+++ nb.js
@@ -1,7 +1,9 @@
 let BACK = 1
+let triple = false
 let history = []
 let correctPosC = 0
 let correctColC = 0
+let correctLetC = 0
 let total = 0
 window.active = false
 let timeRemaining;
@@ -11,12 +13,16 @@ let lastReply
 const colors = [
   "--blue", 
   "--orange", 
-  "--lavender", 
+  "--yellow",
+  "--pink", 
   "--maroon", 
-  "--navy", 
+  "--green", 
   "--grey", 
+  "--brown", 
   "--white",
 ]; 
+
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "J"]
 
 const progressCircle = document.querySelector(".progress-circle");
 
@@ -24,6 +30,7 @@ if(window.location.search == "?colors"){
   Array.from(document.querySelectorAll(".inner-square")).map((e, i) => {
     console.log(i)
     e.style.borderColor = `var(${colors[i]})`;
+    e.style.backgroundColor = `var(${colors[i]})`;
   })
 }
 
@@ -32,12 +39,13 @@ function getRandomSolarizedColor() {
 }
 
 function resetReply() {
-  lastReply = { position: false, color: false };
+  lastReply = { position: false, color: false, letter: false };
 }
 
 function resetEverything(){
   history = [];
   correctPosC = 0;
+  correctLetC = 0;
   correctColC = 0;
   total = 0;
   resetReply();
@@ -48,8 +56,16 @@ Array.from(document.querySelectorAll('.inner-square')).map(e => {
     if(window.active){
       return;
     }
-    BACK = +(e.id.replace("d", ""))+1;
-    document.getElementById("top-button").textContent = BACK;
+    new_back = +(e.id.replace("d", ""))+1;
+    if(BACK == new_back){
+      triple = !triple;
+    }
+    BACK = new_back;
+    let text = BACK
+    if(triple){
+      text += ` (triple)`
+    }
+    document.getElementById("top-button").textContent = text;
     resetEverything();
     ev.stopPropagation();
   })
@@ -72,14 +88,20 @@ function showResults(){
   }
   const pctPos = 100*correctPosC / total;
   const pctCol = 100*correctColC / total;
+  const pctLet = 100*correctLetC / total;
   const modalContent = modal.querySelector("#modal-content");
   modalContent.innerHTML = "";
   const p1 = document.createElement("P");
   const p2 = document.createElement("P");
+  const p3 = document.createElement("P");
   p1.innerHTML = `Positions: <span style="float: right;">${correctPosC} / ${total} (${pctPos.toFixed(2)})</span>`;
   p2.innerHTML = `Colors: <span style="float: right;"> ${correctColC} / ${total} (${pctPos.toFixed(2)})</span>`;
+  p3.innerHTML = `Letters: <span style="float: right;"> ${correctLetC} / ${total} (${pctPos.toFixed(2)})</span>`;
   modalContent.appendChild(p1);
   modalContent.appendChild(p2);
+  if(triple){
+    modalContent.appendChild(p3);
+  }
 }
 
 function startStop(e) {
@@ -87,6 +109,10 @@ function startStop(e) {
   console.info('Start/stop');
   const squares = Array.from(document.querySelectorAll(".inner-square"));
   if(window.active){
+    if(triple){
+      document.getElementById("bottom-button").classList.remove("front");
+      document.getElementById("bottom-button").classList.add("back");
+    }
     document.getElementById("left-button").classList.add("back");
     document.getElementById("right-button").classList.add("back");
     document.getElementById("middle-button").classList.add("back");
@@ -98,11 +124,16 @@ function startStop(e) {
     history = [] // Reset history, so it starts again from the beginning.
     // TODO the button text should not show until we can answer
     squares.map(e => e.style.borderColor = "var(--slighty-lighter-dark-background)");
+    squares.map(e => e.style.backgroundColor = "var(--slighty-lighter-dark-background)");
     document.getElementById("modal").classList.remove("back");
     document.getElementById("modal").classList.add("front");
     showResults();
   } else {
-    document.getElementById("top-button").textContent = BACK;
+    let text = BACK
+    if(triple){
+      text += ` (triple)`
+    }
+    document.getElementById("top-button").textContent = text;
     document.getElementById("middle-button").classList.add("front");
     document.getElementById("middle-button").classList.remove("back");
     document.getElementById("modal").classList.add("back");
@@ -110,6 +141,7 @@ function startStop(e) {
     stepping();
     window.active = setInterval(stepping, 4000);
     squares.map(e => e.style.borderColor = "black");
+    squares.map(e => e.style.backgroundColor = "black");
   }
 }
 
@@ -122,6 +154,10 @@ function showAnswerButtons(){
   document.getElementById("right-button").classList.add("front");
   document.getElementById("left-button").classList.remove("back");
   document.getElementById("right-button").classList.remove("back");
+  if(triple){
+    document.getElementById("bottom-button").classList.add("front");
+    document.getElementById("bottom-button").classList.remove("back");
+  }
 }
 
 document.getElementById('left-button').addEventListener('click', function(e) {
@@ -136,10 +172,21 @@ document.getElementById('right-button').addEventListener('click', function(e) {
   console.info('Same color');
   lastReply.color = !lastReply.color;
   const color = lastReply.color ? "var(--lighter-dark-background)" : "";
-  console.log(lastReply.color, color);
   document.getElementById("right-button").querySelector("p").style.backgroundColor = color;
   e.stopPropagation();
 });
+
+document.getElementById('bottom-button').addEventListener('click', function(e) {
+  if(!triple){
+    return;
+  }
+  console.info('Same letter');
+  lastReply.letter = !lastReply.letter;
+  const color = lastReply.letter ? "var(--lighter-dark-background)" : "";
+  document.getElementById("bottom-button").querySelector("p").style.backgroundColor = color;
+  e.stopPropagation();
+});
+
 
 const coin = () => Math.random() < 0.3; // Kind of coin
 
@@ -147,6 +194,7 @@ const coin = () => Math.random() < 0.3; // Kind of coin
 function generateStep(previous){
   let position = Math.floor(Math.random() * 9);
   let color = Math.floor(Math.random() * colors.length);
+  let letter = Math.floor(Math.random() * letters.length);
   if(previous){
     if(coin()){
       position = previous.position;
@@ -154,10 +202,14 @@ function generateStep(previous){
     if(coin()){
       color = previous.color;
     }
+    if(coin()){
+      letter = previous.letter;
+    }
   }
   return {
     position: position,
-    color: color
+    color: color,
+    letter: letter
   };
 }
 
@@ -166,12 +218,14 @@ function render(step){
   const position = step.position;
   const element = document.getElementById(`d${position}`);
   element.style.borderColor = `var(${colorCSS})`;
+  element.style.backgroundColor = `var(${colorCSS})`;
 }
 
 function unrender(step){
   const position = step.position;
   const element = document.getElementById(`d${position}`);
   element.style.borderColor = `black`;
+  element.style.backgroundColor = `black`;
 }
 
 function score(toScore={kind: "none", increase: 0}){
@@ -185,6 +239,10 @@ function score(toScore={kind: "none", increase: 0}){
   if(toScore.kind == "col"){
     correctColC += score;
     selector = "right";
+  }
+  if(toScore.kind == "let"){
+    correctLetC += score;
+    selector = "bottom";
   }
   document.getElementById(`${selector}-button`).querySelector("p").style.backgroundColor = `var(--sd-${color})`;
 }
@@ -224,12 +282,31 @@ function checkReply(){
       score({kind: "col", increase: 0});
     }
   }
+  if(triple){
+    if(last.letter== previous.letter){
+      if(lastReply.letter){
+        score({kind: "let", increase: 1});
+      } else {
+        score({kind: "let", increase: 0});
+      }
+    } else {
+      if(!lastReply.letter){
+        score({kind: "let", increase: 1});
+      } else {
+        score({kind: "let", increase: 0});
+      }
+    }
+
+  }
   console.info(`pos: ${correctPosC} col: ${correctColC}`);
 }
 
 function resetAnswers(){
   document.getElementById("right-button").querySelector("p").style.backgroundColor = "rgba(0,0,0,0)";
   document.getElementById("left-button").querySelector("p").style.backgroundColor = "rgba(0,0,0,0)";
+  if(triple){
+    document.getElementById("bottom-button").querySelector("p").style.backgroundColor = "rgba(0,0,0,0)";
+  }
 }
 
 function stepping(){
@@ -264,6 +341,16 @@ function stepping(){
   progressContainer.style.left = `${containerLeft}px`;
   progressContainer.style.top = `${containerTop}px`;
 
+  if(triple){
+    const letter = document.getElementById("letter")
+    letter.textContent = letters[step.letter];
+    const letterBB = letter.getBoundingClientRect();
+    const shiftV = letterBB.height/2
+    const shiftH = letterBB.width/2
+    letter.style.left = `${containerLeft+shiftH}px`;
+    letter.style.top = `${containerTop+shiftV}px`;
+    letter.style.color = `var(${colorCSS})`;
+  }
   history.push(step);
   setTimeout(() => render(step), 300);
   resetReply();
